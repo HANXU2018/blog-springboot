b52089367da5a8abcdbe168a872a1f2582902136
HANXU2018 <1076998404@qq.com>
2020-05-24 17:03:04 +0800

    mybatis

--- src/main/java/com/lou/springboot/Application.java
+++ src/main/java/com/lou/springboot/Application.java
@@ -1,5 +1,6 @@
 package com.lou.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@MapperScan("com.lou.springboot.dao") //添加 @Mapper 注解
 public class Application {
 
     public static void main(String[] args) {
