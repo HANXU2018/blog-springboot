58a5710fad27456286726ff61196e40580f72dd1
HANXU2018 <1076998404@qq.com>
2020-05-25 08:28:43 +0800

    kaptcha

---
+++ src/main/java/com/lou/springboot/controller/KaptchaController.java
@@ -0,0 +1,60 @@
+package com.lou.springboot.controller;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+
+@Controller
+public class KaptchaController {
+
+    @Autowired
+    private DefaultKaptcha captchaProducer;
+
+    @GetMapping("/kaptcha")
+    public void defaultKaptcha(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        byte[] captchaOutputStream;
+        ByteArrayOutputStream imgOutputStream = new ByteArrayOutputStream();
+        try {
+            //生产验证码字符串并保存到session中
+            String verifyCode = captchaProducer.createText();
+            System.out.println("验证码"+verifyCode);
+            httpServletRequest.getSession().setAttribute("verifyCode", verifyCode);
+            BufferedImage challenge = captchaProducer.createImage(verifyCode);
+            ImageIO.write(challenge, "jpg", imgOutputStream);
+        } catch (IllegalArgumentException e) {
+            httpServletResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        captchaOutputStream = imgOutputStream.toByteArray();
+        httpServletResponse.setHeader("Cache-Control", "no-store");
+        httpServletResponse.setHeader("Pragma", "no-cache");
+        httpServletResponse.setDateHeader("Expires", 0);
+        httpServletResponse.setContentType("image/jpeg");
+        ServletOutputStream responseOutputStream = httpServletResponse.getOutputStream();
+        responseOutputStream.write(captchaOutputStream);
+        responseOutputStream.flush();
+        responseOutputStream.close();
+    }
+    @GetMapping("/verify")
+    @ResponseBody
+    public String verify(@RequestParam("code") String code, HttpSession session) {
+        if (StringUtils.isEmpty(code)) {
+            return "验证码不能为空";
+        }
+        String kaptchaCode = session.getAttribute("verifyCode") + "";
+        if (StringUtils.isEmpty(kaptchaCode) || !code.equals(kaptchaCode)) {
+            return "验证码错误";
+        }
+        return "验证成功";
+    }
+}
