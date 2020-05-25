cf620debad10e6200e467d96149a0d5eb405f93c
HANXU2018 <1076998404@qq.com>
2020-05-25 23:24:54 +0800

    categories

--- src/main/java/com/lou/springboot/controller/admin/CategoryController.java
+++ src/main/java/com/lou/springboot/controller/admin/CategoryController.java
@@ -2,16 +2,15 @@ package com.lou.springboot.controller.admin;
 
 import com.lou.springboot.common.Result;
 import com.lou.springboot.common.ResultGenerator;
+import com.lou.springboot.entity.BlogCategory;
 import com.lou.springboot.service.CategoryService;
 import com.lou.springboot.utils.PageQueryUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @Controller
@@ -19,6 +18,12 @@ import java.util.Map;
 public class CategoryController {
     @Resource
     private CategoryService categoryService;
+    @GetMapping("/categories")
+    public String categoryPage(HttpServletRequest request) {
+        System.out.println(">>>>>>>>>>>>String categoryPage");
+        request.setAttribute("path", "categories");
+        return "admin/category";
+    }
     /**
      * 分类列表
      */
@@ -31,4 +36,79 @@ public class CategoryController {
         PageQueryUtil pageUtil = new PageQueryUtil(params);
         return ResultGenerator.genSuccessResult(categoryService.getBlogCategoryPage(pageUtil));
     }
+
+    /**
+     * 分类添加
+     */
+    @RequestMapping(value = "/categories/save", method = RequestMethod.POST)
+    @ResponseBody
+    public Result save(@RequestParam("categoryName") String categoryName,
+                       @RequestParam("categoryIcon") String categoryIcon) {
+        if (StringUtils.isEmpty(categoryName)) {
+            return ResultGenerator.genFailResult("请输入分类名称！");
+        }
+        if (StringUtils.isEmpty(categoryIcon)) {
+            return ResultGenerator.genFailResult("请选择分类图标！");
+        }
+        if (categoryService.saveCategory(categoryName, categoryIcon)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult("分类名称重复");
+        }
+    }
+    /**
+     * 分类删除
+     */
+    @RequestMapping(value = "/categories/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public Result delete(@RequestBody Integer[] ids) {
+        if (ids.length < 1) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        if (categoryService.deleteBatch(ids)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult("删除失败");
+        }
+    }
+
+    /**
+     * 分类修改
+     */
+    @RequestMapping(value = "/categories/update", method = RequestMethod.POST)
+    @ResponseBody
+    public Result update(@RequestParam("categoryId") Integer categoryId,
+                         @RequestParam("categoryName") String categoryName,
+                         @RequestParam("categoryIcon") String categoryIcon) {
+        if (categoryId == null || categoryId < 1) {
+            return ResultGenerator.genFailResult("非法参数！");
+        }
+        if (StringUtils.isEmpty(categoryName)) {
+            return ResultGenerator.genFailResult("请输入分类名称！");
+        }
+        if (StringUtils.isEmpty(categoryIcon)) {
+            return ResultGenerator.genFailResult("请选择分类图标！");
+        }
+        if (categoryService.updateCategory(categoryId, categoryName, categoryIcon)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult("分类名称重复");
+        }
+    }
+
+    /**
+     * 详情
+     */
+    @GetMapping("/categories/info/{id}")
+    @ResponseBody
+    public Result info(@PathVariable("id") Integer id) {
+        if (id == null || id < 1) {
+            return ResultGenerator.genFailResult("非法参数！");
+        }
+        BlogCategory category = categoryService.selectById(id);
+        return ResultGenerator.genSuccessResult(category);
+    }
+
+
+
 }
